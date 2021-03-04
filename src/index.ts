86347819b645a8966ac4a147ced493b17cc7ddd3
xiaojun996 <blueskyzx@icloud.com>
2021-03-04 19:29:29 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hello world!!!!!!!!!')
+import { NumMatrix } from './example/leetcode'
+
+const obj = new NumMatrix([[-1]])
+const param_1 = obj.sumRegion.apply(obj, [0, 0, 0, 0])
+
+console.info(param_1)
