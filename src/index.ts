d48e61125a3d9f731c7ae8c1bc29aa7c68205ecf
xiaojun996 <blueskyzx@icloud.com>
2021-05-07 00:03:31 +0800

    chore

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,23 @@ import { traverse } from 'estraverse'
 import { Program, Node } from 'estree'
 import { dots } from 'cli-spinners'
 import chalk from 'chalk'
+import ora from 'ora'
 
 import { clearTerminal, log } from './common/utils'
-
-clearTerminal()
 
 const code = `function getUser() {}`
 
 const ast: Program = parseScript(code)
 
-log(dots)
+const spinner = ora('Loading unicorns').start()
+
+setImmediate(() => {
+  spinner.color = 'yellow'
+  spinner.text = 'Loading rainbows'
+  spinner.spinner = dots
+}, 1000)
+
+clearTerminal()
 
 traverse(ast, {
   // traverse 方法中有 enter 和 leave 两个钩子函数
