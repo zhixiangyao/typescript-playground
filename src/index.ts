6b4c3f6fda778071221db19f42f4b4f37c11108e
xiaojun996 <blueskyzx@icloud.com>
2021-05-08 11:18:45 +0800

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,34 @@
 import { parseScript } from 'esprima'
 import { traverse } from 'estraverse'
+import { generate } from 'escodegen'
 import { Program, Node } from 'estree'
-import { dots } from 'cli-spinners'
 import chalk from 'chalk'
 
-import { clearTerminal, log, anime } from './common/utils'
+import { clearTerminal, log } from './common/utils'
+
+clearTerminal()
 
 const code = `function getUser() {}`
 const ast: Program = parseScript(code)
 
-anime('red', 'haha', dots)
-clearTerminal()
+log(chalk.green.bold('Ast:'), ast, '\n')
+
+log(chalk.green.bold('Old:'), chalk.yellow(generate(ast)), '\n')
 
 traverse(ast, {
   // traverse 方法中有 enter 和 leave 两个钩子函数
   enter(node: Node): void {
     log(chalk.red(`enter -> node.type: ${node.type}`))
+
+    if (node.type === 'Identifier') {
+      node.name = 'getBroker'
+    }
   },
   leave(node: Node): void {
-    log(chalk.green(`leave -> node.type: ${node.type}`))
+    log(chalk.blue(`leave -> node.type: ${node.type}`))
   },
 })
+
+log()
+
+log(chalk.green.bold('New:'), chalk.yellow(generate(ast)))
