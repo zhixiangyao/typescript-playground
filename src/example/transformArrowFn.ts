5f167fe8c5e3562d390dce6edfebdffa928d5350
xiaojun996 <blueskyzx@icloud.com>
2021-05-25 16:28:54 +0800

    refactor

--- src/example/transformArrowFn.ts
+++ src/example/transformArrowFn.ts
@@ -6,41 +6,37 @@ import { chalk, log } from '@common/index'
 
 const transformArrowFn = () => {
   // 转换后 const fn = function(a, b) { return a + b }
-  const originCode = `const fn = (a, b) => a + b;`
+  const code = `const fn = (a, b) => a + b;`
 
-  log(chalk.green('old => '), originCode)
+  log(chalk.green('old => '), code)
 
-  const arrowFnPlugin = (): PluginItem => {
-    return {
-      // 访问者模式
-      visitor: {
-        // 当访问到某个路径的时候进行匹配
+  const arrowFnPlugin = (): PluginItem => ({
+    visitor: {
+      // 当访问到某个路径的时候进行匹配
+      ArrowFunctionExpression(path: NodePath<types.ArrowFunctionExpression>) {
+        const { node } = path //      节点 node: (a, b) => a + b
+        const { params } = node //    函数的参数
+        const { body }: any = node // 二进制表达式: a + b
 
-        ArrowFunctionExpression(path: NodePath<types.ArrowFunctionExpression>) {
-          const { node } = path //      节点 node: (a, b) => a + b
-          const { params } = node //    函数的参数
-          const { body }: any = node // 二进制表达式: a + b
+        // returnStatement
+        const newReturnStatement = returnStatement(body)
 
-          // returnStatement
-          const newReturnStatement = returnStatement(body)
+        // blockStatement
+        const newBlockStatement = blockStatement([newReturnStatement])
 
-          // blockStatement
-          const newBlockStatement = blockStatement([newReturnStatement])
+        // functionExpression
+        const newFunctionExpression = functionExpression(null, params, newBlockStatement)
 
-          // functionExpression
-          const newFunctionExpression = functionExpression(null, params, newBlockStatement)
-
-          path.replaceWith(newFunctionExpression)
-        },
+        path.replaceWith(newFunctionExpression)
       },
-    }
-  }
+    },
+  })
 
-  const babelData: BabelFileResult | null = transform(originCode, {
+  const data: BabelFileResult | null = transform(code, {
     plugins: [arrowFnPlugin],
   })
 
-  log(chalk.red('new => '), babelData?.code)
+  log(chalk.red('new => '), data?.code)
 }
 
 export default transformArrowFn
