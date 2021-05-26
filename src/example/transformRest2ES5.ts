39bb27cfd07cfdae6dd5f455d1aa38976e0d9e1c
xiaojun996 <blueskyzx@icloud.com>
2021-05-26 15:39:34 +0800

    refactor: 只使用 babel/core

--- src/example/transformRest2ES5.ts
+++ src/example/transformRest2ES5.ts
@@ -1,5 +1,3 @@
-import { default as generate } from '@babel/generator'
-import { default as traverse, NodePath } from '@babel/traverse'
 import {
   isArrayExpression,
   memberExpression,
@@ -10,8 +8,9 @@ import {
   variableDeclaration,
 } from '@babel/types'
 import { VariableDeclaration, SpreadElement } from '@babel/types'
+import { PluginItem, BabelFileResult, NodePath } from '@babel/core'
+import { transform } from '@babel/core'
 import { default as chalk } from 'chalk'
-import { parse } from '@babel/parser'
 
 import { log } from '@common/index'
 
@@ -20,45 +19,49 @@ import { log } from '@common/index'
  * @param code const arr = [ ...arr1, ...arr2 ];
  * @returns var arr = [].concat(arr1, arr2)
  */
-const transformRest2ES5 = (code = `const arr = [ ...arr1, ...arr2 ];`): string => {
-  const AST = parse(code)
-
+const transformRest2ES5 = (code = `const arr = [ ...arr1, ...arr2 ];`): string | null | undefined => {
   log(chalk.green.bold('old =>'))
   log(code)
 
-  traverse(AST, {
-    VariableDeclaration(path: NodePath<VariableDeclaration>) {
-      const { node } = path //         节点: const arr = [ ...arr1, ...arr2 ];
-      const { declarations } = node // declarations: arr = [ ...arr1, ...arr2 ];
-      const kind = 'var'
+  const transformRest2ES5Plugin = (): PluginItem => {
+    return {
+      visitor: {
+        VariableDeclaration(path: NodePath<VariableDeclaration>) {
+          const { node } = path //         节点: const arr = [ ...arr1, ...arr2 ];
+          const { declarations } = node // declarations: arr = [ ...arr1, ...arr2 ];
+          const kind = 'var'
 
-      // 边界判定
-      if (node.kind !== kind && declarations.length === 1 && isArrayExpression(declarations[0].init)) {
-        const args: SpreadElement[] = declarations[0].init.elements.map(item => {
-          const { argument }: any = item
-          return argument
-        })
-        // [].concat()
-        const callee = memberExpression(arrayExpression(), identifier('concat'))
-        // [].concat(arr1, arr2)
-        const init = callExpression(callee, args)
-        // arr = [].concat(arr1, arr2)
-        const declaration = variableDeclarator(declarations[0].id, init)
-        // var arr = [].concat(arr1, arr2)
-        const newVariableDeclaration = variableDeclaration(kind, [declaration])
-        path.replaceWith(newVariableDeclaration)
-      }
-    },
+          // 边界判定
+          if (node.kind !== kind && declarations.length === 1 && isArrayExpression(declarations[0].init)) {
+            const args: SpreadElement[] = declarations[0].init.elements.map(item => {
+              const { argument }: any = item
+              return argument
+            })
+            // [].concat()
+            const callee = memberExpression(arrayExpression(), identifier('concat'))
+            // [].concat(arr1, arr2)
+            const init = callExpression(callee, args)
+            // arr = [].concat(arr1, arr2)
+            const declaration = variableDeclarator(declarations[0].id, init)
+            // var arr = [].concat(arr1, arr2)
+            const newVariableDeclaration = variableDeclaration(kind, [declaration])
+            path.replaceWith(newVariableDeclaration)
+          }
+        },
+      },
+    }
+  }
+
+  const data: BabelFileResult | null = transform(code, {
+    plugins: [transformRest2ES5Plugin()],
   })
-
-  const newCode = generate(AST).code
 
   // 转换后
   // var arr = [].concat(arr1, arr2)
   log(chalk.red.bold('New =>'))
-  log(newCode)
+  log(data?.code)
 
-  return newCode
+  return data?.code
 }
 
 export default transformRest2ES5
