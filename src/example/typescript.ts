481c94628050bdca7985f6b85d3635c5635a8689
zhixiangyao <blueskyzx@icloud.com>
2021-07-15 17:08:09 +0800

    add: 体操 + 1

--- src/example/typescript.ts
+++ src/example/typescript.ts
@@ -17,3 +17,22 @@ const user: User = {
 }
 
 console.log(user)
+
+// 表演体操
+const extend = <T extends object, U extends object>(first: T, second: U): T & U => {
+  const result = <T & U>{}
+  for (const id in first) {
+    ;(<T>result)[id] = first[id]
+  }
+  for (const id in second) {
+    if (!result.hasOwnProperty(id)) {
+      ;(<U>result)[id] = second[id]
+    }
+  }
+
+  return result
+}
+
+const x = extend({ a: 'hello' }, { b: 42 })
+
+console.log(x)
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,3 @@
 import { clearTerminal } from '@common/index'
 
 clearTerminal()
-
-const extend = <T extends object, U extends object>(first: T, second: U): T & U => {
-  const result = <T & U>{}
-  for (const id in first) {
-    ;(<T>result)[id] = first[id]
-  }
-  for (const id in second) {
-    if (!result.hasOwnProperty(id)) {
-      ;(<U>result)[id] = second[id]
-    }
-  }
-
-  return result
-}
-
-const x = extend({ a: 'hello' }, { b: 42 })
-
-console.log(x)
