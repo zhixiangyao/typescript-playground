322a639a360eb00ad47aed7326fa0536351153b9
zhixiangyao <blueskyzx@icloud.com>
2021-09-24 16:33:02 +0800

    chore

--- test/index.ts
+++ test/index.ts
@@ -8,6 +8,24 @@ const clearTerminal = () => {
 
 clearTerminal()
 
-import { transformFnName } from '../src'
+function fn(num: number) {
+  return function <T extends { new (...arg: any[]): {} }>(constructor: T) {
+    return class extends constructor {
+      json = { a: num }
+    }
+  }
+}
 
-transformFnName()
+@fn(12)
+class User {
+  json: {
+    a: number
+  }
+}
+
+const obj = new User()
+obj.json.a = 80
+console.log(obj.json)
+
+const obj2 = new User()
+console.log(obj2.json)
