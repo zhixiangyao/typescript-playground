50bfeafa45625fdf569db0ff349318dd0b543d58
zhixiangyao <blueskyzx@icloud.com>
2021-10-28 11:22:53 +0800

    chore

--- test/index.ts
+++ test/index.ts
@@ -5,8 +5,10 @@ import 'reflect-metadata'
 clearTerminal()
 
 const ClassDecorator = (num: number) => {
-  return <T extends { new (...arg: any[]): {} }>(constructor: T) => {
-    return class extends constructor {
+  return <T extends { new (...arg: any[]): {} }>(target: T) => {
+    // 在类上定义元数据，key 为 `classMetaData`，value 为 `aaaa`
+    Reflect.defineMetadata('classMetaData', 'aaaa', target)
+    return class extends target {
       json = { a: num }
     }
   }
@@ -23,6 +25,9 @@ const MethodDecorator = (): MethodDecorator => {
     console.log(`Method: ${methodName}, Method   type: ${type.name}`)
     paramtypes.forEach(({ name }, index) => console.log(`Method: ${methodName}, param[${index}] type: ${name}`))
     console.log(`Method: ${methodName}, return   type: ${returntype.name}`)
+
+    // 在类的原型属性 'someMethod' 上定义元数据，key 为 `methodMetaData`，value 为 `bbbb`
+    Reflect.defineMetadata('methodMetaData', 'bbbb', target, propertyKey)
   }
 }
 
@@ -44,4 +49,7 @@ const obj2 = new User()
 console.log(obj2.json) // { a: 12 }
 
 console.log(Reflect.getMetadata('inClass', User)) // 'A'
+console.log(Reflect.getMetadata('classMetaData', User)) // 'aaaa'
+
 console.log(Reflect.getMetadata('inMethod', new User(), 'hello')) // 'B'
+console.log(Reflect.getMetadata('methodMetaData', new User(), 'hello')) // 'bbbb'
