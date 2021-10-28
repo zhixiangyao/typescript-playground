3d5d68fcb91f05223cc596df7259dec0cae4fa63
zhixiangyao <blueskyzx@icloud.com>
2021-10-28 11:03:08 +0800

    chore

--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,6 @@
 import { clearTerminal } from './utils'
-import 'reflect-metadata'
 import { isSymbolObject } from 'util/types'
+import 'reflect-metadata'
 
 clearTerminal()
 
@@ -14,12 +14,15 @@ const ClassDecorator = (num: number) => {
 
 const MethodDecorator = (): MethodDecorator => {
   return (target: Object, propertyKey: string | symbol, descriptor) => {
+    const methodName = isSymbolObject(propertyKey) ? propertyKey.toString() : propertyKey
+
     const type = Reflect.getMetadata('design:type', target, propertyKey) as Function
     const paramtypes = Reflect.getMetadata('design:paramtypes', target, propertyKey) as Function[]
-    const methodName = isSymbolObject(propertyKey) ? propertyKey.toString() : propertyKey
-    console.log(`Method: ${methodName}, type: ${type.name}`)
+    const returntype = Reflect.getMetadata('design:returntype', target, propertyKey) as Function
 
+    console.log(`Method: ${methodName}, Method   type: ${type.name}`)
     paramtypes.forEach(({ name }, index) => console.log(`Method: ${methodName}, param[${index}] type: ${name}`))
+    console.log(`Method: ${methodName}, return   type: ${returntype.name}`)
   }
 }
 
@@ -32,8 +35,8 @@ class User {
 
   @Reflect.metadata('inMethod', 'B')
   @MethodDecorator()
-  public hello(name: string): string {
-    return 'hello world' + name
+  public hello(name: string, age: number): string {
+    return 'hello world, hi~ my name is' + name + ', ' + age + '.'
   }
 }
 
