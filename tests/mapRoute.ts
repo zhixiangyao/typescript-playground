10479ad90884fcc5e0cfd7b11f160e7d6afd496e
zhixiangyao <blueskyzx@icloud.com>
2025-06-03 11:54:11 +0800

    feat: 完善例子

--- tests/mapRoute.ts
+++ tests/mapRoute.ts
@@ -1,68 +1,55 @@
 import { isConstructor, isFunction } from './utils'
 import 'reflect-metadata'
 
-enum METHOD {
+enum ENUM_METHOD {
   GET = 'GET',
   POST = 'POST',
   PUT = 'PUT',
   DELETE = 'DELETE',
 }
 
-enum METADATA_KEY {
+enum ENUM_METADATA_KEY {
   METHOD = 'method',
   PATH = 'path',
 }
 
 const Controller = (path: string): ClassDecorator => {
   return target => {
-    // defineMetadata has 4 params!
-    // metadataKey: METADATA_KEY.PATH,
-    // metadataValue: path,
-    // target: target,
-    // propertyKey: undefined
-    Reflect.defineMetadata(METADATA_KEY.PATH, path, target)
+    Reflect.defineMetadata(ENUM_METADATA_KEY.PATH, path, target)
   }
 }
 
 const createMappingDecorator =
-  (method: keyof typeof METHOD) =>
+  (method: ENUM_METHOD) =>
   (path: string): MethodDecorator => {
     return (target, key, descriptor) => {
-      // MethodDecorator: target === SomeClass.prototype
-      // ClassDecorator: target === SomeClass
-      // descriptor.value === (target as unknown)[key] === [Function someXXXMethod]
-
-      Reflect.defineMetadata(METADATA_KEY.PATH, path, descriptor.value as unknown as object)
-      Reflect.defineMetadata(METADATA_KEY.METHOD, method, descriptor.value as unknown as object)
+      Reflect.defineMetadata(ENUM_METADATA_KEY.PATH, path, descriptor.value as unknown as object)
+      Reflect.defineMetadata(ENUM_METADATA_KEY.METHOD, method, descriptor.value as unknown as object)
     }
   }
 
-const Get = createMappingDecorator(METHOD.GET)
-const Post = createMappingDecorator(METHOD.POST)
-const Put = createMappingDecorator(METHOD.PUT)
+const Get = createMappingDecorator(ENUM_METHOD.GET)
+const Post = createMappingDecorator(ENUM_METHOD.POST)
+const Put = createMappingDecorator(ENUM_METHOD.PUT)
 
 const routeGenerator = <T extends object>(instance: T) => {
-  const prototype = Object.getPrototypeOf(instance)
-
-  // prototype['constructor'] === instance.constructor
-  const rootRoutePath = Reflect.getMetadata(METADATA_KEY.PATH, prototype['constructor'])
-
-  // 筛选出类的 methodName
-  const methodsNames = Object.getOwnPropertyNames(prototype).filter(
-    item => !isConstructor(item) && isFunction(prototype[item])
+  const target = Object.getPrototypeOf(instance)
+  const rootRoutePath = Reflect.getMetadata(ENUM_METADATA_KEY.PATH, target['constructor'])
+  const propertyKeys = Object.getOwnPropertyNames(target).filter(
+    item => !isConstructor(item) && isFunction(target[item])
   )
 
-  return methodsNames.map(methodName => {
-    const fn = prototype[methodName] as Function
+  return propertyKeys.map(propertyKey => {
+    const func = target[propertyKey] as Function
+    const routePath = Reflect.getMetadata(ENUM_METADATA_KEY.PATH, func)
+    const method = Reflect.getMetadata(ENUM_METADATA_KEY.METHOD, func)
 
-    // 取出定义的 metadata
-    const routePath = Reflect.getMetadata(METADATA_KEY.PATH, fn)
-    const method = Reflect.getMetadata(METADATA_KEY.METHOD, fn)
     return {
       route: `${rootRoutePath}${routePath}`,
       method,
-      fn,
-      methodName,
+      func,
+      funcName: propertyKey,
+      funcPramTypes: Reflect.getMetadata('design:paramtypes', target, propertyKey),
     }
   })
 }
@@ -70,7 +57,7 @@ const routeGenerator = <T extends object>(instance: T) => {
 @Controller('/test')
 class SomeClass {
   @Get('/a')
-  someGetMethod() {
+  someGetMethod(body: object) {
     return 'hello world'
   }
 
@@ -85,37 +72,4 @@ class SomeClass {
   }
 }
 
-/**
- * [
- *   {
- *     route: '/test/a',
- *     method: 'GET',
- *     fn: [Function: someGetMethod],
- *     methodName: 'someGetMethod'
- *   },
- *   {
- *     route: '/test/b',
- *     method: 'POST',
- *     fn: [Function: somePostMethod],
- *     methodName: 'somePostMethod'
- *   }
- *   {
- *     route: '/test/c',
- *     method: 'PUT',
- *     fn: [Function: somePutMethod],
- *     methodName: 'somePutMethod'
- *   }
- * ]
- */
 console.log(routeGenerator<SomeClass>(new SomeClass()))
-
-/**
- * Reflect.metadata:
- *
- * function metadata(metadataKey, metadataValue) {
- *   function decorator(target, propertyKey) {
- *     Reflect.defineMetadata(metadataKey, metadataValue, target, propertyKey);
- *   }
- *   return decorator;
- * }
- */
